7d54e17237a0b522dc00b56bee53a6e8248b3d04
Tiffany Ho <tiffanieho@outlook.com>
2025-01-27 21:38:10 +0000

    Add pages/index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,35 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import UploadImage from '../components/UploadImage';
+import ImageModifier from '../components/ImageModifier';
+
+const queryClient = new QueryClient();
+
+const Home: React.FC = () => {
+  useEffect(() => {
+    const container = document.getElementById('root');
+    if (container) {
+      const root = createRoot(container);
+      root.render(
+        <QueryClientProvider client={queryClient}>
+          <HomeContent />
+        </QueryClientProvider>
+      );
+    }
+  }, []);
+
+  return <div id="root"></div>;
+};
+
+const HomeContent: React.FC = () => {
+  return (
+    <div>
+      <h1>Modify Your Image</h1>
+      <UploadImage />
+      <ImageModifier />
+    </div>
+  );
+};
+
+export default Home;
