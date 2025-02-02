979cc28863981769fbbaf52073402f7999598081
tiffjai <96027851+tiffjai@users.noreply.github.com>
2025-02-02 21:07:36 +0000

    Deploying to gh-pages from @ tiffjai/my-face-app@063670c7cd6e5491758b5192fdf299457ccc73f5 🚀

--- _next/static/jtXRUJbHsHhT8LmsvpKan/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/17e61fb3-a02b32272decc6cc.js","static/chunks/782-3257aa0d905fce20.js","static/chunks/pages/index-93675efe984c7630.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
